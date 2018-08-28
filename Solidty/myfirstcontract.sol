a0572900f1e1d7e20c26e1445f30761068451834
danwise91 <danielwise19918@gmail.com>
2018-08-28 17:31:52 -0400

    all files rdy

---
+++ Solidty/myfirstcontract.sol
@@ -0,0 +1,84 @@
+pragma solidity ^0.4.0;
+
+interface Regulator {
+    function checkValue(uint amount) external returns (bool);
+    function loan() external returns (bool);
+}
+
+contract Bank is Regulator {
+    uint private value;
+    address private owner;
+    
+    modifier ownerFunc {
+        require(owner == msg.sender);
+        _;
+    }
+
+    constructor(uint amount)  public {
+        value = amount;
+        owner = msg.sender;
+    }
+    
+    function deposit(uint amount)  public ownerFunc {
+        value += amount;
+    }
+    
+    function withdraw(uint amount)  public ownerFunc {
+        if (checkValue(amount)) {
+            value -= amount;
+        }
+    }
+    
+    function balance()  public view returns (uint) {
+        return value;
+    }
+    
+    function checkValue(uint amount)  public returns (bool) {
+        // Classic mistake in the tutorial value should be above the amount
+        return value >= amount;
+    }
+    
+    function loan()  public returns   (bool) {
+        return value > 0;
+    }
+}
+
+contract MyFirstContract is Bank(10) {
+    string private name;
+    uint private age;
+    
+    function setName(string newName)  public {
+        name = newName;
+    }
+    
+    function getName()  public view returns (string) {
+        return name;
+    }
+    
+    function setAge(uint newAge)  public {
+        age = newAge;
+    }
+    
+    function getAge()  public view returns (uint) {
+        return age;
+    }
+}
+
+contract TestThrows {
+    function  testAssert()  pure public {
+        assert(1 == 2);
+    }
+    
+    function   testRequire() pure  public {
+        require(2 == 1);
+    }
+    
+    function   testRevert()  pure public {
+        revert();
+    }
+    
+    function   testThrow() pure   public  {
+        throw;
+    }
+}
+
